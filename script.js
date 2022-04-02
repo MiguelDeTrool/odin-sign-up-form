566e2318c7aa7d76b021072b27455975502b1c43
MiguelDeTrool <migueldetrool@gmail.com>
2022-04-02 19:27:22 +0200

    Improve responsiveness, fix validation parameters, start password match validation script

---
+++ script.js
@@ -0,0 +1,24 @@
+
+const label = document.getElementById("password-label");
+console.log(label);
+/*
+
+const passInput1 = document.querySelector("#user-password");
+const passInput2 = document.querySelector("#user-password-confirm");
+
+passInput1.addEventListener('change', checkMatchingPasswords);
+passInput2.addEventListener('change', checkMatchingPasswords);
+
+function checkMatchingPasswords(e) {
+    if (passInput1.value != passInput2.value) {
+        console.log("not same")
+        label.style.visibility = visible;
+        passInput1.style.borderColor = blue;
+    }
+    else {
+        label.style.visibility = hidden;
+
+    }
+}
+
+*/
